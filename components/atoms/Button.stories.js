e2a9c20398d2b71859a77dcd40adda3c1321df8a
Ryoko Kawashima <ry.sep27@gmail.com>
2020-10-21 00:46:45 +0900

    add storybook

---
+++ components/atoms/Button.stories.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Button',
+}
+
+export const Default = () => '<Button label="Button"/>'
+export const Info = () => '<Button label="Button" type="info"/>'
+export const Warning = () => '<Button label="Button" type="warning"/>'
+export const Danger = () => '<Button label="Button" type="danger"/>'
---
+++ components/atoms/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button :class="getStyle(type)" @click="onClick">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+type Type = 'info' | 'warning' | 'danger'
+
+export default Vue.extend({
+  name: 'Button',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'default',
+    },
+    onClick: {
+      type: Function,
+      required: false,
+      default: () => {},
+    },
+  },
+  computed: {
+    getStyle() {
+      return function (type: Type): String {
+        switch (type) {
+          case 'info':
+            return 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+          case 'warning':
+            return 'bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded'
+          case 'danger':
+            return 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+          default:
+            return 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        }
+      }
+    },
+  },
+})
+</script>
