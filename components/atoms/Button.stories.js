a8faa895489d12aecc167888dfae2d326ace26a5
Ryoko Kawashima <ry.sep27@gmail.com>
2020-10-21 21:49:27 +0900

    refactoring

--- components/atoms/Button.stories.js
+++ components/atoms/Button.stories.js
@@ -3,6 +3,9 @@ export default {
 }
 
 export const Default = () => '<Button label="Button"/>'
-export const Info = () => '<Button label="Button" type="info"/>'
+export const Primary = () => '<Button label="Button" type="primary"/>'
 export const Warning = () => '<Button label="Button" type="warning"/>'
 export const Danger = () => '<Button label="Button" type="danger"/>'
+export const Black = () => '<Button label="Button" type="black"/>'
+export const Small = () => '<Button label="Button" size="small"/>'
+export const RoundFull = () => '<Button label="Button" :roundFull="true"/>'
