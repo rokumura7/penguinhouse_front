363da0b4f725d3e438bdec08108ff72af90a592b
rokumura7 <rokumura.8807@gmail.com>
2021-01-13 01:40:41 +0900

    fixed index

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,10 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">penguinhouse_front</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="flex items-center text-center min-h-screen h-screen">
+    <div class="w-full">
+      <div class="flex justify-center">
+        <Logo src="logo2.png" size="l" />
       </div>
+      <h1 class="title block font-light tracking-widest">Penguin House</h1>
     </div>
   </div>
 </template>
@@ -37,34 +21,8 @@ export default Vue.extend({})
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
+  font-family: 'Comic Sans MS';
   color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
