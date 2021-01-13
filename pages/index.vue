3975e7a6cb7782287ab14d93b1b72cd8dec7e6b3
rokumura7 <rokumura.8807@gmail.com>
2021-01-14 02:06:05 +0900

    added tailwindcss.config.js

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
       <div class="flex justify-center">
         <Logo src="logo2.png" size="l" />
       </div>
-      <h1 class="title block font-light tracking-widest">Penguin House</h1>
+      <h1
+        class="text-main-darkest font-cosmic block font-light tracking-widest"
+      >
+        Penguin House
+      </h1>
     </div>
   </div>
 </template>
@@ -14,15 +18,3 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.title {
-  font-family: 'Comic Sans MS';
-  color: #526488;
-}
-</style>
